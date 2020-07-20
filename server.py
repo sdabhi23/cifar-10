14564cecb66f8592bf1da639b82e6d4cfbfd8c94
Shrey <shrey.dabhi23@gmail.com>
2020-07-20 17:46:29 +0530

    upgrade model and environment
    * add model training notebook
    * migrate deployment to tensorflow 2.x
    * update node and python versions
    * fix model path
    * improve cleanup of artifacts

--- server.py
+++ server.py
@@ -1,34 +1,23 @@
 from flask import Flask, jsonify, request, abort, render_template, send_file
-from tensorflow.compat.v1 import get_default_graph, Session
-from tensorflow.python.keras.backend import set_session
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
-import tensorflow.compat.v1.logging as logs
 from PIL import Image
 import numpy as np
 
-logs.set_verbosity(logs.ERROR)
-
-sess = Session()
-graph = get_default_graph()
-set_session(sess)
-cifar10_model = load_model('model.h5')
+cifar10_model = load_model('MobileNetV2__2020-07-20-10-32-55.h5')
 
 app = Flask(__name__)
 
 classes = ['airplane', 'automobile', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-
 def predict(img):
     img = img.resize((32, 32), Image.ANTIALIAS)
     img = img.convert('RGB')
     x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    with graph.as_default():
-        set_session(sess)
-        preds = cifar10_model.predict(x)[0]
-        return {classes[x]: float(preds[x]) for x in np.argsort(preds)[::-1][:3]}
+    x = np.expand_dims(x, axis=0) / 255.
+    preds = cifar10_model.predict(x)[0]
+    return {classes[x]: float(preds[x]) for x in np.argsort(preds)[::-1][:3]}
 
 
 @app.route("/")
